7faae1e443c903dd2417f37646968d6143600064
Sujeet K <rajrock7254@gmail.com>
2025-06-15 12:52:02 +0530

    updated path

--- vite.config.ts
+++ vite.config.ts
@@ -3,18 +3,17 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// Replace with your GitHub repo name
-const repoName = "personal-portfolio";
-
+// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: mode === 'development' ? '/' : `/${repoName}/`,
+  base: '/',
   server: {
     host: "::",
     port: 8080,
   },
   plugins: [
     react(),
-    mode === 'development' && componentTagger(),
+    mode === 'development' &&
+    componentTagger(),
   ].filter(Boolean),
   resolve: {
     alias: {
